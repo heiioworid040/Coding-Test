b805222bf0e5f64b2b453088ed245b04ce7ce455
노은주 <heiio0.4.0worid@gmail.com>
2023-04-21 18:18:09 +0900

    230421 18:17 단어의 개수

---
+++ beakjoon2/src/stage5/Test8_20230421.java
@@ -0,0 +1,15 @@
+package stage5;
+
+import java.io.*;
+import java.util.*;
+
+public class Test8_20230421 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		String s=br.readLine();
+		StringTokenizer st=new StringTokenizer(s, " ");
+		
+		System.out.println(st.countTokens());
+		br.close();
+	}
+}
