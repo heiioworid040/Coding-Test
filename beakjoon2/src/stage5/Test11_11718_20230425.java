07f361eb12a46b4f4d71b08baee1fe9c92ca8017
노은주 <heiio0.4.0worid@gmail.com>
2023-04-25 18:47:50 +0900

    230425 18:47 그대로 출력하기

---
+++ beakjoon2/src/stage5/Test11_11718_20230425.java
@@ -0,0 +1,16 @@
+package stage5;
+
+import java.io.*;
+
+public class Test11_11718_20230425 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		String s;
+		
+		while((s=br.readLine())!=null) {
+			System.out.println(s);
+		}
+		
+		br.close();
+	}
+}
