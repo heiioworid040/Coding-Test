952392c15c7105339931d1d091cd73dfb82eec63
노은주 <heiio0.4.0worid@gmail.com>
2023-04-27 20:46:56 +0900

    230427 20:46 킹, 퀸, 룩, 비숍, 나이트, 폰

---
+++ beakjoon2/src/stage6/Test7_3003_230427.java
@@ -0,0 +1,18 @@
+package stage6;
+
+import java.io.*;
+import java.util.*;
+
+public class Test7_3003_230427 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st=new StringTokenizer(br.readLine()," ");
+		
+		int[] chess=new int[]{1, 1, 2, 2, 2, 8};
+		for(int i=0;i<chess.length;i++) {
+			int dong=chess[i]-Integer.parseInt(st.nextToken());
+			System.out.print(dong+" ");
+		}
+		br.close();
+	}
+}
