d8022859e1cf84eb6df18c32f78ccff853a6fdcb
노은주 <heiio0.4.0worid@gmail.com>
2023-06-03 22:47:04 +0900

    230603 22:40 배수와 약수

--- beakjoon2/src/stage9/Problem_5086_230603.java
+++ beakjoon2/src/stage9/Problem_5086_230603.java
@@ -14,7 +14,7 @@ public class Problem_5086_230603 {
             st=new StringTokenizer(br.readLine());
             num1=Integer.parseInt(st.nextToken());
             num2=Integer.parseInt(st.nextToken());
-            if(num1==num2) {                 //0이 2개 주어질 때만 두 수가 같음
+            if(num1==num2) {                 //마지막 줄에 0이 두 개 주어짐 (이를 제외하고 두 수가 같은 경우는 없음)
             	break;                       //while문 종료
             }else if(num2%num1==0) {         //약수라면
                 sb.append("factor"+"\n");
