cbcd148c12dfedfd5ab4be15787483c874c06fdf
노은주 <heiio0.4.0worid@gmail.com>
2023-06-22 20:56:20 +0900

    230622 2056 직사각형

---
+++ beakjoon2/src/stage10/Problem_27323_230622.java
@@ -0,0 +1,13 @@
+package stage10;
+
+import java.io.*;
+
+public class Problem_27323_230622 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int a = Integer.parseInt(br.readLine());
+		int b = Integer.parseInt(br.readLine());
+		
+		System.out.println(a*b);
+	}
+}
