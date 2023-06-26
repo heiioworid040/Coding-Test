6094c66285d916dd482617d50256098152136f0c
노은주 <heiio0.4.0worid@gmail.com>
2023-06-26 21:20:03 +0900

    230626 2119 수학은 체육과목 입니다

---
+++ beakjoon2/src/stage10/Problem_15894_230626.java
@@ -0,0 +1,13 @@
+package stage10;
+
+import java.io.*;
+
+public class Problem_15894_230626 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		long n = Long.parseLong(br.readLine()) * 4;
+		//10^9 = 100억
+		
+		System.out.println(n);
+	}
+}
