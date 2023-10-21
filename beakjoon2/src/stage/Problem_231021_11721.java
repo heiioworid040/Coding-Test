db42aeaf7029c867e1487a86aa8ca97cae7b5166
노은주 <heiio0.4.0worid@gmail.com>
2023-10-21 23:12:52 +0900

    231021 2312 알파벳 개수

---
+++ beakjoon2/src/stage/Problem_231021_11721.java
@@ -0,0 +1,27 @@
+package stage;
+
+import java.io.*;
+
+public class Problem_231021_11721 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		// 소문자로만 이루어진 단어
+		String word = br.readLine();
+		
+		// 알파벳 배열
+		int[] alp = new int[26];
+		
+		// 각 알파벳의 개수 계산
+		for(char c : word.toCharArray()) {
+			alp[c - 97]++;
+		}
+		
+		// 알파벳 출력
+		for(int i : alp) {
+			sb.append(i).append(" ");
+		}
+		System.out.println(sb);
+	}
+}
