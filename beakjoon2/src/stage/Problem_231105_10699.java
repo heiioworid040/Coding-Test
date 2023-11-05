7f7b85ec6d5200b5e00b2b036062bad85a39047b
노은주 <heiio0.4.0worid@gmail.com>
2023-11-05 20:28:45 +0900

    231105 2028 오늘 날짜

---
+++ beakjoon2/src/stage/Problem_231105_10699.java
@@ -0,0 +1,12 @@
+package stage;
+
+import java.time.*;
+
+public class Problem_231105_10699 {
+	public static void main(String[] args) {
+		// yyyy-MM-dd
+		LocalDate now = LocalDate.now();
+		// 출력
+		System.out.println(now);
+	}
+}
