73e62445568509c9983c889fbe79d81187ee4e35
ujnue <ujnue@192.168.35.129>
2023-11-17 21:20:09 +0900

    231117 2120 대소문자 바꾸기

---
+++ beakjoon2/src/stage/Problem_231117_2744.java
@@ -0,0 +1,26 @@
+package stage;
+
+import java.io.*;
+
+public class Problem_231117_2744 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String word = br.readLine();
+		String changeWord = reverseCase(word);
+		System.out.println(changeWord);
+    }
+	
+	// 대문자는 소문자, 소문자는 대문자로 변환하는 메서드
+	public static String reverseCase(String word) {
+		StringBuilder changeWord = new StringBuilder(word.length());
+		for(int i = 0; i < word.length(); i++) {
+			char c = word.charAt(i);
+			if('a' <= c && 'z' >= c) {
+				changeWord.append(Character.toUpperCase(c));
+			}else {
+				changeWord.append(Character.toLowerCase(c));
+			}
+		}
+		return changeWord.toString();
+	}
+}
