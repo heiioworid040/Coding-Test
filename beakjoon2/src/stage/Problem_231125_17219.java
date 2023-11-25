40a4b9b0b575821d65f9c42bf352ac3b69182fa3
ujnue <ujnue@192.168.35.129>
2023-11-25 20:21:11 +0900

    231125 2021 비밀번호 찾기

---
+++ beakjoon2/src/stage/Problem_231125_17219.java
@@ -0,0 +1,33 @@
+package stage;
+
+import java.io.*;
+import java.util.*;
+
+public class Problem_231125_17219 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int siteInfoCount = Integer.parseInt(st.nextToken());
+		int findSiteCount = Integer.parseInt(st.nextToken());
+		
+		// 입력된 사이트 정보를 저장하는 맵
+		Map<String, String> siteInfo = new HashMap<>();
+		
+		// 사이트 정보 입력 부분
+		while(siteInfoCount-- > 0) {
+			st = new StringTokenizer(br.readLine());
+			String site = st.nextToken();
+			String password = st.nextToken();
+			siteInfo.put(site, password);
+		}
+		
+		// 해당 사이트 주소의 비밀번호 찾기
+		while(findSiteCount-- > 0) {
+			String findSite = br.readLine();
+			if(siteInfo.containsKey(findSite)) {
+				System.out.println(siteInfo.get(findSite));
+			}
+		}
+	}
+}
